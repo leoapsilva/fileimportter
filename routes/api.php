a85c145edb7e8dc1977a758bba5980574227785c
Leonardo Alves de Paula e Silva <leoapsilva@gmail.com>
2021-01-17 14:02:55 -0300

    RestAPI issue #21

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\ImportFileApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->get( '/import-files', 'Api\ApiImportFileApiController@index');
+
+Route::middleware('auth:api')->get( '/import-files/{id}', 'Api\ApiImportFileController@show');
